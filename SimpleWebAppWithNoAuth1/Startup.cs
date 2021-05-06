eba240da0edf8d1c628b04ce10488ad2e95c9a42
v-yuwzh <v-yuwzh@valshaos19001>
2021-05-05 23:25:14 -0700

    123

--- SimpleWebAppWithNoAuth1/Startup.cs
+++ SimpleWebAppWithNoAuth1/Startup.cs
@@ -1,9 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using SimpleWebAppWithNoAuth1.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -23,7 +25,9 @@ namespace SimpleWebAppWithNoAuth1
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<BloggingContext>(options => options.UseSqlServer(Configuration.GetConnectionString("BlogConnection")));
             services.AddControllersWithViews().AddRazorRuntimeCompilation();
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -48,6 +52,7 @@ namespace SimpleWebAppWithNoAuth1
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapRazorPages();
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
